a698b573bf4b74968a7328ad06491077223324b5
DeveshDutt2710 <dd123.ues2017@gmail.com>
2021-02-18 06:16:28 +0530

    added models to admin.py

--- admin_api/admin.py
+++ admin_api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
+admin.site.register(Profiles)
+admin.site.register(Products)
+admin.site.register(Orders)
+admin.site.register(Coupons)
+admin.site.register(Enquiries)
